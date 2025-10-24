88a6340b32ac18c6f4d03d6c8bdcc4a48ffaf8a3
wsk0715 <leindey@gmail.com>
2025-10-24 17:45:58 +0900

    fix: Fruit 타입 import 오류 수정
    - fruitGenerator.ts에서 Fruit 타입 import 제거
    - 런타임 오류 해결

--- src/utils/fruitGenerator.ts
+++ src/utils/fruitGenerator.ts
@@ -1,5 +1,5 @@
 import Matter from 'matter-js';
-import { FRUITS, Fruit } from '../constants/fruits';
+import { FRUITS } from '../constants/fruits';
 
 export class FruitGenerator {
   private world: Matter.World;
